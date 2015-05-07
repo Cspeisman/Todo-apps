a75ee5beed745f718654302530b779d3c9dfba5f
Cspeisman <Cspeisman@gmail.com>
2015-05-07 17:45:05 -0400

    .

--- django_todo_api/tasks/urls.py
+++ django_todo_api/tasks/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     # ex: /tasks/
-    url(r'^$', views.index),
+    url(r'^$', views.TaskList().as_view()),
     # ex: /tasks/4
-    url(r'^(?P<task_id>[0-9]+)/$', views.detail),
+    url(r'^(?P<pk>[0-9]+)/$', views.TaskDetail().as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- django_todo_api/tasks/views.py
+++ django_todo_api/tasks/views.py
@@ -1,20 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core import serializers
 from .models import Tasks
-from .forms import TasksForm
+from .serializers import TasksSerializer
+from rest_framework import generics
 
-# Create your views here.
-def index(request):
-    data = serializers.serialize('json', Tasks.objects.all())
-    return HttpResponse(data, content_type='application/json')
+class TaskList(generics.ListCreateAPIView):
+    queryset = Tasks.objects.all()
+    serializer_class = TasksSerializer
 
-def detail(request, task_id):
-    print(request)
-    task = Tasks.objects.get(pk=task_id)
-    if request.POST:
-        form = TasksForm(request.POST, instance=task)
-        form.save()
-    data = Tasks.objects.filter(pk=task_id)
-    serialize_data = serializers.serialize('json', data)
-    return HttpResponse(serialize_data, content_type='application/json')
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tasks.objects.all()
+    serializer_class = TasksSerializer
